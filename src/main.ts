41f31dcfc600d51819212d99a4de5ff92db043dc
Bohdan Kachur <bohdan.kachur@magnise.com>
2021-07-10 00:29:40 +0300

    Update with terraform

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   app.setGlobalPrefix("api/v1");
 
   const options = new DocumentBuilder()
-    .setTitle("Notes todo app 2")
+    .setTitle("Notes todo app 4")
     .setDescription(
       "A documentation for notes"
     )
